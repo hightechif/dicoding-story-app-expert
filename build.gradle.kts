752df5222beb2e85165faa23356cfe3580338a61
R. Fadhil <ridhanfadhilah@gmail.com>
2024-11-03 00:38:36 +0700

    ENHANCE: Update buildscript and plugins dependency with new kotlin DSL standard

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.1")
+        classpath(libs.navigation.safeargs)
     }
 }
 
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.mapsplatform) apply false
 }
 
 tasks.register("clean", Delete::class) {
